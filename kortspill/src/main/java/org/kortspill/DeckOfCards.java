9843251252642f71b61c8957f6a9ec40ffe6fcd2
Vetle Brøske Reinton <vetlerein@gmail.com>
2025-03-15 22:32:48 +0100

    PlayingCard & DeckOfCards

---
+++ kortspill/src/main/java/org/kortspill/DeckOfCards.java
@@ -0,0 +1,20 @@
+package org.kortspill;
+
+import java.util.ArrayList;
+
+public class DeckOfCards {
+    
+    private final char[] suit = {'S', 'H', 'D', 'C'};
+    private final char[] value = {'2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A'};
+    private final ArrayList<PlayingCard> cardDeck = new ArrayList<>();
+
+    public DeckOfCards() {
+    }
+
+    private void dealHand(int n){
+
+        
+
+    }
+
+}
