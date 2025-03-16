8150de6a846dff610237684f6da3fe30a1554f58
Vetle Brøske Reinton <vetlerein@gmail.com>
2025-03-17 00:44:18 +0100

    Main.java update

--- kortspill/src/main/java/org/kortspill/Main.java
+++ kortspill/src/main/java/org/kortspill/Main.java
@@ -1,13 +1,14 @@
 package org.kortspill;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.ButtonBar;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
-import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
@@ -16,53 +17,57 @@ public class Main extends Application{
     public static void main(String[] args) {
         launch(args);
     }
+    
+    Stage window;
+    Button knapp = new Button();
+    Button byttVinduKnapp = new Button("Bytt vindu");
+    Line line = new Line();; 
+    Text text = new Text("Kortspill");;
 
     @Override
-    public void start(Stage stage) throws Exception {
+    public void start(Stage window) throws Exception {
         
-        Group root = new Group();
-        Scene scene = new Scene(root, Color.LIGHTSKYBLUE);
+        AnchorPane layout2 = new AnchorPane();
+        Group layout = new Group();
+        Scene scene = new Scene(layout, Color.LIGHTSKYBLUE);
+        Scene scene2 = new Scene(layout2);
         
-        Text text = new Text("Kortspill");
         text.setX(10);
         text.setY(45);
-        text.setFont(Font.font("Calibri", 30));
+        text.setFont(Font.font("Times new roman", 30));
         text.setFill(Color.BLACK);
-        
-        Line line = new Line();
+
         line.setStartX(10);
         line.setStartY(70);
         line.setEndX(775);
         line.setEndY(70);
         line.setStrokeWidth(5);;
         line.setStroke(Color.BLUE);
-        
-        Button button = new Button();
-        button.setMinWidth(200);
-        button.setMinHeight(300);
-        button.setScaleX(1);
-        button.setScaleY(1);
-        button.setText("Hello");
 
-        Rectangle rectangle = new Rectangle();
-       
-        root.getChildren().add(text);
-        root.getChildren().add(line);
-        root.getChildren().add(button);
+        knapp.setText("Hello");
+        knapp.setLayoutX(20);
+        knapp.setLayoutY(90);
+        knapp.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Test");
+            }
+        });
+    
+        byttVinduKnapp.setLayoutX(70);
+        byttVinduKnapp.setLayoutY(90);
+        byttVinduKnapp.setOnAction(e -> {
+            window.setScene(scene2);
+        });
 
-        rectangle.setX(0);
-        rectangle.setY(0);
-        rectangle.setWidth(0);
-        rectangle.setHeight(0);
-        rectangle.setFill(null);
+        layout.getChildren().addAll(text, line, knapp, byttVinduKnapp);
 
-
-        stage.setScene(scene);
-        stage.setTitle("Kortspill");
-        stage.setWidth(800);
-        stage.setHeight(600);
-        stage.setResizable(false);
-        stage.show();
+        window.setScene(scene);
+        window.setTitle("Kortspill");
+        window.setWidth(800);
+        window.setHeight(600);
+        window.setResizable(false);
+        window.show();
 
     }
 }
