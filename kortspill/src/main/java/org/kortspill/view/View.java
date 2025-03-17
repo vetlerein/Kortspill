5cbd477e6677cefeea2c418151fae7cfbe6bee60
Vetle <vetlerein@gmail.com>
2025-03-17 15:26:17 +0100

    Lagde CSS og fikset opp i View.java

--- kortspill/src/main/java/org/kortspill/view/View.java
+++ kortspill/src/main/java/org/kortspill/view/View.java
@@ -1,16 +1,14 @@
 package org.kortspill.view;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.Group;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Line;
-import javafx.scene.text.Font;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class View extends Application{
@@ -19,56 +17,82 @@ public class View extends Application{
     }
     
     Stage window;
-    Button knapp = new Button();
-    Button byttVinduKnapp = new Button("Bytt vindu");
-    Line line = new Line();; 
-    Text text = new Text("Kortspill");;
+    VBox mainLayout;
+    HBox kortBox;
+    HBox underKortBox;
+    StackPane cardLayout;
 
     @Override
     public void start(Stage window) throws Exception {
         
-        AnchorPane layout2 = new AnchorPane();
-        Group layout = new Group();
-        Scene scene = new Scene(layout, Color.LIGHTSKYBLUE);
-        Scene scene2 = new Scene(layout2);
+        //Lager layouts
+        mainLayout = new VBox();
+        mainLayout.setSpacing(10);
+        mainLayout.setPadding(new Insets(10));
         
-        text.setX(10);
-        text.setY(45);
-        text.setFont(Font.font("Times new roman", 30));
-        text.setFill(Color.BLACK);
+        kortBox = new HBox();
+       //TODO kortBox.setStyle(Color.LIGHTSKYBLUE.toString());
+        kortBox.setSpacing(10);
+        
+        underKortBox = new HBox();
+        underKortBox.setSpacing(10);
+        
+        Scene scene = new Scene(mainLayout, 800, 600, Color.LIGHTSKYBLUE);
 
-        line.setStartX(10);
-        line.setStartY(70);
-        line.setEndX(775);
-        line.setEndY(70);
-        line.setStrokeWidth(5);;
-        line.setStroke(Color.BLUE);
+        //Legger til 5 kort (StackPane) i kortBox i det programmet starter
+        for (int i = 0; i < 5; i++) {
+            StackPane card = new StackPane();
+            card.setPrefSize(200, 300);
+            card.setStyle("-fx-background-color: white; -fx-border-color: black;");
+            kortBox.getChildren().add(card);
+        }
 
-        knapp.setText("Hello");
-        knapp.setLayoutX(20);
-        knapp.setLayoutY(90);
-        knapp.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Test");
+        //Legger til 5 kort (StackPane) i kortBox ved å trykke på knappen
+        Button dealButton = new Button("Deal");
+        dealButton.setPrefSize(70, 35);
+        dealButton.setOnAction(e -> {
+            kortBox.getChildren().clear();
+            for (int i = 0; i < 5; i++) {
+                StackPane card = new StackPane();
+                card.setPrefSize(200, 300);
+                card.setStyle("-fx-background-color: white; -fx-border-color: black;");
+                kortBox.getChildren().add(card);
             }
         });
-    
-        byttVinduKnapp.setLayoutX(70);
-        byttVinduKnapp.setLayoutY(90);
-        byttVinduKnapp.setOnAction(e -> {
-            window.setScene(scene2);
-        });
 
-        layout.getChildren().addAll(text, line, knapp, byttVinduKnapp);
+        //Tekst vedsiden av knappen
+        Label label = new Label("You won");
+        label.setFont(javafx.scene.text.Font.font("Calibri", 30));
+
+        //Legger ting i layouts
+        underKortBox.getChildren().addAll(dealButton, label);
+        mainLayout.getChildren().addAll(kortBox, underKortBox);
 
         window.setScene(scene);
         window.setTitle("Kortspill");
-        window.setWidth(800);
-        window.setHeight(600);
         window.setResizable(false);
         window.show();
-
     }
 }
 
+        // mainLayout.getStylesheets().add(Objects.requireNonNull(getClass()
+        //       .getResource("/org/kortspill/style.css")).toExternalForm());
+
+        // mainLayout.getStyleClass().add("mainLayout");
+
+        // text.setX(10);
+        // text.setY(45);
+        // text.setFont(Font.font("Times new roman", 30));
+        // text.setFill(Color.BLACK);
+
+                // line.setStartX(10);
+        // line.setStartY(70);
+        // line.setEndX(775);
+        // line.setEndY(70);
+        // line.setStrokeWidth(5);;
+        // line.setStroke(Color.BLUE);
+
+        // knapp.setText("Hello");
+        // knapp.setLayoutX(20);
+        // knapp.setLayoutY(90);
+        // knapp.setOnAction(e -> System.out.println("Test"));
